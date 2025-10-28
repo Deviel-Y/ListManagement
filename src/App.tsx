358388f88cd01dd920612acb15844ca10cb05a6c
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-28 23:29:48 +0330

    Add components temporarily to app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import ModalTriggerButton from "./components/Modal/ModalTriggerButton";
+import type { ListType } from "./components/Table/TableBody";
+import TableContainer from "./components/Table/TableContainer";
+
 function App() {
- return <div className="text-xl">hello World</div>;
+ return (
+  <main className="h-dvh flex flex-row items-center justify-center">
+   <TableContainer>
+    <TableContainer.TableTitle
+     className="font-semibold py-2 w-full text-center text-xl"
+     label="this is a label"
+    />
+
+    <TableContainer.TableTopContent className="w-full p-2">
+     <ModalTriggerButton />
+    </TableContainer.TableTopContent>
+
+    <TableContainer.TableBody
+     tableColumnLabels={["title", "subtitle", "createdAt", "action"]}
+     lists={lists}
+    />
+   </TableContainer>
+  </main>
+ );
 }
 
 export default App;
+
+const lists: ListType[] = [
+ { title: "title1", subtitle: "subtitle1", createdAt: "createdAt1" },
+ { title: "title2", subtitle: "subtitle2", createdAt: "createdAt2" },
+ { title: "title3", subtitle: "subtitle3", createdAt: "createdAt3" },
+ { title: "title4", subtitle: "subtitle4", createdAt: "createdAt4" },
+];
