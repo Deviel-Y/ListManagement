f8f85f0a3a2bcf69ea9c1d16b574ce15ab21438c
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-29 15:07:07 +0330

    Display list table on "/"

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
 import ModalTriggerButton from "./components/Modal/ModalTriggerButton";
-import type { ListType } from "./components/Table/TableBody";
 import TableContainer from "./components/Table/TableContainer";
 
 function App() {
- return (
-  <main className="h-dvh flex flex-row items-center justify-center">
-   <TableContainer>
-    <TableContainer.TableTitle
-     className="font-semibold py-2 w-full text-center text-xl"
-     label="this is a label"
-    />
+  return (
+    <main className="h-dvh flex flex-col items-center justify-center">
+      <TableContainer>
+        <TableContainer.TableTitle label="List management" />
 
-    <TableContainer.TableTopContent className="w-full p-2">
-     <ModalTriggerButton />
-    </TableContainer.TableTopContent>
+        <TableContainer.TableTopContent>
+          <ModalTriggerButton buttonLabel="Create new list" varient="primary" />
+        </TableContainer.TableTopContent>
 
-    <TableContainer.TableBody
-     tableColumnLabels={["title", "subtitle", "createdAt", "action"]}
-     lists={lists}
-    />
-   </TableContainer>
-  </main>
- );
+        <TableContainer.TableBody />
+      </TableContainer>
+    </main>
+  );
 }
 
 export default App;
-
-const lists: ListType[] = [
- { title: "title1", subtitle: "subtitle1", createdAt: "createdAt1" },
- { title: "title2", subtitle: "subtitle2", createdAt: "createdAt2" },
- { title: "title3", subtitle: "subtitle3", createdAt: "createdAt3" },
- { title: "title4", subtitle: "subtitle4", createdAt: "createdAt4" },
-];
