218606507faedb3595b636c18c1bfd23fd45f0b2
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-29 13:16:07 +0330

    Add error messages to zod validation on undefined values

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -4,12 +4,12 @@ export type ListSchemaType = z.infer<typeof listSchema>;
 
 export const listSchema = z.object({
   title: z
-    .string()
+    .string("Title is required")
     .min(1, "Title is required")
     .max(50, "Title must be less than 50 characters long"),
 
   subtitle: z
-    .string()
+    .string("Subtitle is required")
     .min(1, "Subtitle is required")
     .max(200, "Subtitle must be less than 200 characters long"),
 });
