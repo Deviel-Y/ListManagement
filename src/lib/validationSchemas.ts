523cc3da95a9ea8635f569ee7c33de82e328deda
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-29 12:55:36 +0330

    Define list schema using zod

---
+++ src/lib/validationSchemas.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export type ListSchemaType = z.infer<typeof listSchema>;
+
+export const listSchema = z.object({
+  title: z
+    .string()
+    .min(1, "Title is required")
+    .max(50, "Title must be less than 50 characters long"),
+
+  subtitle: z
+    .string()
+    .min(1, "Subtitle is required")
+    .max(200, "Subtitle must be less than 200 characters long"),
+});
