a9b4cf800c525f9bb16f57438d2383f5d8083fa1
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-29 21:06:13 +0330

    Make list title unique

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -11,122 +11,126 @@ import FormErrorMessage from "./FormErrorMessage";
 import Input from "./Input";
 
 interface Props {
-  listToEdit?: ListType;
-  onClose: (modalState: boolean) => void;
+ listToEdit?: ListType;
+ onClose: (modalState: boolean) => void;
 }
 
 const Form = ({ listToEdit, onClose }: Props) => {
-  const [isDisable, setIsDisable] = useState<boolean>(false);
-  const addToList = useListStore((s) => s.addToList);
-  const editList = useListStore((s) => s.editList);
+ const [isDisable, setIsDisable] = useState<boolean>(false);
+ const addToList = useListStore((s) => s.addToList);
+ const editList = useListStore((s) => s.editList);
+ const lists = useListStore((s) => s.lists);
 
-  const fakeApiCall = <T,>(data: T, delay = 1000): Promise<T> =>
-    new Promise((resolve) => setTimeout(() => resolve(data), delay));
+ const fakeApiCall = <T,>(data: T, delay = 1000): Promise<T> =>
+  new Promise((resolve) => setTimeout(() => resolve(data), delay));
 
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<ListSchemaType>({
-    resolver: zodResolver(listSchema),
-  });
+ const {
+  control,
+  handleSubmit,
+  formState: { errors },
+ } = useForm<ListSchemaType>({
+  resolver: zodResolver(listSchema),
+ });
 
-  return (
-    <form
-      onSubmit={handleSubmit((data) => {
-        setIsDisable(true);
+ return (
+  <form
+   onSubmit={handleSubmit((data) => {
+    setIsDisable(true);
 
-        if (listToEdit) {
-          const updatedList = fakeApiCall<
-            ListSchemaType & { createdAt: string }
-          >({
-            createdAt: listToEdit.createdAt,
-            title: data.title,
-            subtitle: data.subtitle,
-          });
+    const list = lists.find((list) => list.title === data.title);
+    if (list)
+     return (
+      toast.error("List with this title is already exists."),
+      setIsDisable(false)
+     );
 
-          return toast
-            .promise(updatedList, {
-              loading: "Editting...",
-              error: "Couldn't edit the list",
-              success: "Successfully editted",
-            })
-            .then((res) => {
-              editList(res);
-              onClose(false);
-            })
-            .finally(() => setIsDisable(false));
-        }
+    if (listToEdit) {
+     const updatedList = fakeApiCall<ListSchemaType & { createdAt: string }>({
+      createdAt: listToEdit.createdAt,
+      title: data.title,
+      subtitle: data.subtitle,
+     });
 
-        const newList = fakeApiCall<ListSchemaType & { createdAt: string }>({
-          ...data,
-          createdAt: new Date().toLocaleString(),
-        });
-        toast
-          .promise(newList, {
-            loading: "Creating...",
-            error: "Couldn't create the list",
-            success: "Successfully created",
-          })
-          .then((res) => {
-            addToList(res);
-            onClose(false);
-          })
-          .finally(() => setIsDisable(false));
-      })}
-      className="flex flex-col gap-5"
-    >
-      <div className="flex flex-col gap-1 ">
-        <Controller
-          defaultValue={listToEdit?.title}
-          control={control}
-          name="title"
-          render={({ field: { onChange } }) => (
-            <Input
-              defaultValue={listToEdit?.title}
-              label="Title"
-              onChange={onChange}
-            />
-          )}
-        />
+     return toast
+      .promise(updatedList, {
+       loading: "Editting...",
+       error: "Couldn't edit the list",
+       success: "Successfully editted",
+      })
+      .then((res) => {
+       editList(res);
+       onClose(false);
+      })
+      .finally(() => setIsDisable(false));
+    }
 
-        <AnimatePresence>
-          {errors.title && (
-            <FormErrorMessage errorMessage={errors.title.message as string} />
-          )}
-        </AnimatePresence>
-      </div>
-
-      <div className="flex flex-col gap-1">
-        <Controller
-          defaultValue={listToEdit?.subtitle}
-          control={control}
-          name="subtitle"
-          render={({ field: { onChange } }) => (
-            <Input
-              defaultValue={listToEdit?.subtitle}
-              label="Subtitle"
-              onChange={onChange}
-            />
-          )}
-        />
-
-        <AnimatePresence>
-          {errors.subtitle && (
-            <FormErrorMessage
-              errorMessage={errors.subtitle.message as string}
-            />
-          )}
-        </AnimatePresence>
-      </div>
-
-      <Button
-        disable={isDisable}
-        label={listToEdit ? "Edit list" : "Create new list"}
-        type="submit"
+    const newList = fakeApiCall<ListSchemaType & { createdAt: string }>({
+     ...data,
+     createdAt: new Date().toLocaleString(),
+    });
+    toast
+     .promise(newList, {
+      loading: "Creating...",
+      error: "Couldn't create the list",
+      success: "Successfully created",
+     })
+     .then((res) => {
+      addToList(res);
+      onClose(false);
+     })
+     .finally(() => setIsDisable(false));
+   })}
+   className="flex flex-col gap-5"
+  >
+   <div className="flex flex-col gap-1 ">
+    <Controller
+     defaultValue={listToEdit?.title}
+     control={control}
+     name="title"
+     render={({ field: { onChange } }) => (
+      <Input
+       defaultValue={listToEdit?.title}
+       label="Title"
+       onChange={onChange}
       />
-    </form>
-  );
+     )}
+    />
+
+    <AnimatePresence>
+     {errors.title && (
+      <FormErrorMessage errorMessage={errors.title.message as string} />
+     )}
+    </AnimatePresence>
+   </div>
+
+   <div className="flex flex-col gap-1">
+    <Controller
+     defaultValue={listToEdit?.subtitle}
+     control={control}
+     name="subtitle"
+     render={({ field: { onChange } }) => (
+      <Input
+       defaultValue={listToEdit?.subtitle}
+       label="Subtitle"
+       onChange={onChange}
+      />
+     )}
+    />
+
+    <AnimatePresence>
+     {errors.subtitle && (
+      <FormErrorMessage errorMessage={errors.subtitle.message as string} />
+     )}
+    </AnimatePresence>
+   </div>
+
+   <Button
+    disable={isDisable}
+    label={listToEdit ? "Edit list" : "Create new list"}
+    type="submit"
+   />
+  </form>
+ );
 };
 
 export default Form;
