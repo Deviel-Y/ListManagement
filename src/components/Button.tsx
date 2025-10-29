915c10a136ea2d294cca6691be2a8c63873ab2a6
Daniel Yadghar <daniel072.dy@gmail.com>
2025-10-29 20:04:06 +0330

    Add ouline variant to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,51 +1,60 @@
 import { motion } from "motion/react";
 import type { JSX } from "react";
-import type { ModalTriggerButtonVarients } from "./Modal/ModalTriggerButton";
 
 interface ButtonProps {
-  label: string | JSX.Element;
-  onClick?: () => void;
-  varients?: ModalTriggerButtonVarients;
-  type?: "submit" | "button";
-  disable?: boolean;
+ label: string | JSX.Element;
+ onClick?: () => void;
+ varients?: ModalTriggerButtonVarients;
+ type?: "submit" | "button";
+ disable?: boolean;
 }
 
 const Button = ({
-  disable,
-  label,
-  onClick,
-  varients = "primary",
-  type = "button",
+ disable,
+ label,
+ onClick,
+ varients = "primary-solid",
+ type = "button",
 }: ButtonProps) => {
-  return (
-    <motion.button
-      disabled={disable}
-      type={type}
-      className={`${
-        varientMapping[varients!].style
-      } transition-colors cursor-pointer rounded-xl disabled:bg-gray-400 disabled:text-white`}
-      onClick={onClick}
-      initial={{ scale: 1 }}
-      whileTap={{ scale: 0.95 }}
-      transition={{ duration: 0.125 }}
-    >
-      {label}
-    </motion.button>
-  );
+ return (
+  <motion.button
+   disabled={disable}
+   type={type}
+   className={`${
+    varientMapping[varients!].style
+   } transition-colors cursor-pointer rounded-xl disabled:bg-gray-400 disabled:text-white`}
+   onClick={onClick}
+   initial={{ scale: 1 }}
+   whileTap={{ scale: 0.95 }}
+   transition={{ duration: 0.125 }}
+  >
+   {label}
+  </motion.button>
+ );
 };
 
 export default Button;
 
 const varientMapping: Record<ModalTriggerButtonVarients, { style: string }> = {
-  primary: {
-    style: "bg-blue-600 hover:bg-blue-700 px-4 py-1 text-white",
-  },
-  warning: {
-    style:
-      "text-yellow-500 hover:text-yellow-600 font-semibold hover:bg-yellow-400/60 p-2",
-  },
-  danger: {
-    style:
-      "text-red-500 hover:text-red-600 font-semibold hover:bg-red-400/60 p-2",
-  },
+ "primary-solid": {
+  style: "bg-blue-600 hover:bg-blue-700 px-4 py-1 text-white",
+ },
+ "primary-outline": {
+  style:
+   "text-blue-500 hover:text-blue-600 font-semibold hover:bg-blue-400/60 p-2",
+ },
+ warning: {
+  style:
+   "text-yellow-500 hover:text-yellow-600 font-semibold hover:bg-yellow-400/60 p-2",
+ },
+ danger: {
+  style:
+   "text-red-500 hover:text-red-600 font-semibold hover:bg-red-400/60 p-2",
+ },
 };
+
+export type ModalTriggerButtonVarients =
+ | "primary-solid"
+ | "primary-outline"
+ | "warning"
+ | "danger";
--- src/components/Modal/ModalTriggerButton.tsx
+++ src/components/Modal/ModalTriggerButton.tsx
@@ -1,43 +1,41 @@
 import { useState, type JSX } from "react";
 import type { ListType } from "../../store";
-import Button from "../Button";
+import Button, { type ModalTriggerButtonVarients } from "../Button";
 import Form from "../Form";
 import ModalContainer from "./ModalContainer";
 
 interface Props {
-  varient?: ModalTriggerButtonVarients;
-  buttonLabel: string | JSX.Element;
-  listToEdit?: ListType;
+ varient?: ModalTriggerButtonVarients;
+ buttonLabel: string | JSX.Element;
+ listToEdit?: ListType;
 }
 
 const ModalTriggerButton = ({
-  varient = "primary",
-  buttonLabel,
-  listToEdit,
+ varient = "primary-solid",
+ buttonLabel,
+ listToEdit,
 }: Props) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+ const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  return (
-    <>
-      <Button
-        varients={varient}
-        label={buttonLabel}
-        onClick={() => setIsOpen(true)}
-      />
+ return (
+  <>
+   <Button
+    varients={varient}
+    label={buttonLabel}
+    onClick={() => setIsOpen(true)}
+   />
 
-      <ModalContainer isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <ModalContainer.ModalHeader className="text-center mb-5">
-          {listToEdit ? "Edit List Form" : "Create New List Form"}
-        </ModalContainer.ModalHeader>
+   <ModalContainer isOpen={isOpen} onClose={() => setIsOpen(false)}>
+    <ModalContainer.ModalHeader className="text-center mb-5">
+     {listToEdit ? "Edit List Form" : "Create New List Form"}
+    </ModalContainer.ModalHeader>
 
-        <ModalContainer.ModalBody>
-          <Form listToEdit={listToEdit} onClose={() => setIsOpen(false)} />
-        </ModalContainer.ModalBody>
-      </ModalContainer>
-    </>
-  );
+    <ModalContainer.ModalBody>
+     <Form listToEdit={listToEdit} onClose={() => setIsOpen(false)} />
+    </ModalContainer.ModalBody>
+   </ModalContainer>
+  </>
+ );
 };
 
 export default ModalTriggerButton;
-
-export type ModalTriggerButtonVarients = "primary" | "warning" | "danger";
